d66b6c179d151fce4cb4dc2a8b6ab7532f1dcb5b
Dominika Kaźmierczak <dkl.kazmierczak@gmail.com>
2022-02-03 12:41:51 +0100

    Changed link in the footer

--- src/Footer.js
+++ src/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
         </a>
         by{" "}
         <a
-          href="https://lucid-kare-ac1369.netlify.app/index.html"
+          href="https://www.dkkazmierczak.com/index.html"
           target="_blank"
           rel="noreferrer"
           title="Link to Portfolio"
